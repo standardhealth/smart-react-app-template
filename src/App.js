636be93e799fe27e426b610a784e0b39d77eaec0
Dan Lee <daniellee@mitre.org>
2019-10-03 11:12:31 -0400

    Initial SoF app displaying patient name.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const getPatientName = (name = [] ) => {
+  const entry = name.find(n => n.use === 'official') || name[0];
+  return entry ? `${entry.given.join(' ')} ${entry.family}` : 'No name';
+};
+
+const App = ({ patient }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1> {getPatientName(patient.name)} </h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import FHIR from 'fhirclient';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+FHIR.oauth2.init({
+    clientId: 'Input client id you get when you register the app',
+    scope: 'launch/patient openid profile'
+}).then((client) => {
+    client.patient.read().then((patient) => {
+        ReactDOM.render(<App patient={patient} />, rootElement);
+    }, (error) => {
+        console.error(error);
+        ReactDOM.render(<p> {error.stack} </p>, rootElement);
+    });
+});
