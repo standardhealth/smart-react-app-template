3e0a78238eefefe0590ce2e8bf49cfd976a2ef83
Reece Adamson <radamson@mitre.org>
2019-10-22 21:51:23 -0400

    move navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from 'components/Header';
-import Navigation from './Navigation';
+import Navigation from 'components/Navigation';
 import logo from './logo.svg';
 
 // get name from resource
