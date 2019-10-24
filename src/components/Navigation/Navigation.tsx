0435eeae88482978d2a2986174045d80d9536865
Reece Adamson <radamson@mitre.org>
2019-10-24 10:55:21 -0400

    add some comments to Navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -15,11 +15,28 @@ interface Address {
   state?: string;
 }
 
+/***
+ * Returns the first patient address provided as a string.
+ *
+ * @param address - The array of addresses for the patient
+ * @returns The address presented as 'city, state'
+ */
 const getPatientAddress = (address: Array<Address> = []): string => {
   const entry = address[0];
   return entry ? [entry.city, entry.state].filter(item => !!item).join(', ') : 'No Address';
 };
 
+/**
+ * Basic Navigation Element.
+ *
+ * Provides a back arrow, a snaphshot of the patient information, and a dropdown menu
+ *
+ * @param name - The patient's name
+ * @param birthDate - The patient's date of birth
+ * @param gender - The patient's gender
+ * @param address - The patient's address
+ * @constructor
+ */
 const Navigation: React.FC<Props> = ({ name, birthDate, gender, address}) => {
   return (
     <nav className='navigation'>
