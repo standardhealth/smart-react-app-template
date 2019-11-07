a72fc57bb5e3d63ee346103530b67546074f4dc1
kghoreshi <kghoreshi@mitre.org>
2019-11-07 12:04:18 -0500

    add config

---
+++ src/util/ConfigManager.js
@@ -0,0 +1,26 @@
+export default class ConfigManager {
+  constructor(config = {}) {
+    this.config = global.CONFIG ? { ...global.CONFIG , ...config } : { ...config };
+  }
+
+  get(name, defaultValue) {
+    const parts = name.split('.');
+    let context = this.config;
+    let value = null;
+    for (const i in parts) {
+      const part = parts[i];
+      value = context[part];
+      context = value;
+    }
+    return value || defaultValue;
+  }
+
+  add(conf) {
+    if (conf) {
+      this.config = {
+        ...this.config,
+        ...conf
+      };
+    }
+  }
+}
